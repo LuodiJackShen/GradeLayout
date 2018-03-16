ec5d1623384961532278cc669beac436b36e059d
shenmingliang1 <shenmingliang1@jd.com>
2018-03-16 09:35:28 +0800

    add maven support

---
+++ app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.shenmingliang1.demo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- gradelayout/src/main/java/jack/view/GradeLayout.java
+++ gradelayout/src/main/java/jack/view/GradeLayout.java
@@ -1,4 +1,4 @@
-package com.example.shenmingliang1.demo;
+package jack.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
